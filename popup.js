7f2bad3f869b139b8571fa54d92f2010b91c87cd
lyuly <liumike567@gmail.com>
2022-07-17 20:40:15 +0800

    添加注释

--- popup.js
+++ popup.js
@@ -3,12 +3,14 @@ const button = document.getElementById("confirm");
 //清除用
 let id;
 
+// 开始事件
 function todo() {
   if (!id) {
     id = setInterval(getTime, 1000);
   }
 }
 
+// 获取时间
 function getTime() {
   let date = new Date();
   const year = date.getFullYear();
@@ -21,6 +23,7 @@ function getTime() {
   content.innerHTML = `${year}年${month}月${day}日${h}:${min}:${second}`;
 }
 
+// 停止事件
 function stopdo() {
   clearInterval(id);
   id = null;
